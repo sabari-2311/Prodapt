e45a86531663cf09b4ce976ee6d537fc319bde6a
Sabarinathan A <sabarinathan.a@prodapt.com>
2025-08-07 11:30:22 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
   return (
-    <footer className="bg-dark text-light text-center py-3 mt-5">
+    <footer className="bg-dark text-light text-center py-3 mt-auto w-100" style={{ position: 'fixed', bottom: 0, left: 0 }}>
       <div className="container">
         <p className="mb-0">&copy; {new Date().getFullYear()} MyApp. All rights reserved.</p>
       </div>
